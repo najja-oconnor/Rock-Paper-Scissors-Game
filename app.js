312c73fb814ad0f3de8bbc130f1d283ae9ee3c62
Najja O'Connor <najjaoconnor@icloud.com>
2020-04-11 12:40:19 -0400

    made screenshot

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const game = () => {
-    pScore = 0;
+   let pScore = 0;
+   let cScore = 0;
+
+   const startGame = () => {
+       const playBtn = document.querySelector('.intro button')
+       const introScreen = document.querySelector('.intro')
+       const match = document.querySelector('.match')
+
+       playBtn.addEventListener('click', () => {
+           introScreen.classList.add('fadeOut')
+       })
+   }
+
+   //make sure to call all inner functions
+   startGame()
 }
+
+//call game function - start the game
+game()
